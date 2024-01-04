b66967fc5d5ff072f4510a55742185d5c7ee2223
AlexDShyft <151895390+AlexDShyft@users.noreply.github.com>
2024-01-04 18:56:34 -0500

    tet

---
+++ _app/immutable/chunks/paths.f970025c.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1hlh9ni)==null?void 0:s.base)??"/APIDoc/blob/main";var a;const l=((a=globalThis.__sveltekit_1hlh9ni)==null?void 0:a.assets)??e;export{l as a,e as b};
